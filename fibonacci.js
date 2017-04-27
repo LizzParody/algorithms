faa78332c977ca5cdadb13720b8a2a75fd086ba5
Liz Parody <lizparody@gmail.com>
2017-04-26 22:29:32 -0500

    added the fibonacci explanaition

--- fibonacci.js
+++ fibonacci.js
@@ -1,3 +1,6 @@
+//La sucesión de Fibonacci comienza con los números 0 y 1, y a partir de estos,
+//«cada término es la suma de los dos anteriores», es la relación de recurrencia que la define.
+
 function fibonacci(n) {
   if (n === 0 || n === 1) {
     return 1;
