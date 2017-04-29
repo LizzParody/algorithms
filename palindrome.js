ac7f49cf6be9562e6b7b6f67fd61cfba556ec234
Liz Parody <lizparody@gmail.com>
2017-04-28 22:33:03 -0500

    a word or sentence that's spelled the same way both forward and backward

--- palindrome.js
+++ palindrome.js
@@ -1,3 +1,4 @@
+//Check if a word is palindrome (a word or sentence that's spelled the same way both forward and backward)
 function palindrome(word) {
    if (word == word.split("").reverse().join("")){
     return true;
